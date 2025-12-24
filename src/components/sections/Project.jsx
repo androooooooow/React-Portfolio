47e70f7af31fba6406c80f7d40ae270f0ef6c3a6
drewDevs <agojoandrew18@gmail.com>
2025-12-24 19:06:52 +0800

    Update project files

--- src/components/sections/Project.jsx
+++ src/components/sections/Project.jsx
@@ -8,9 +8,9 @@ export const Project =() =>{
                 Featured Projects
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <div className="p-6 ">
-                    <h3>CareSync</h3>
-                    <p>CareSync is a school clinic website that shares health-related 
+                <div className="p-6 rounded-xl border bordder-white/10 hover:translate-y hover:border-blue-500/30 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] transition-all">
+                    <h3 className="text-xl font-bold mb-2">CareSync</h3>
+                    <p className="text-gray-400 mb-4" >CareSync is a school clinic website that shares health-related 
                         essential information and services with students and staff. 
                     </p>
                     <div>
@@ -20,6 +20,40 @@ export const Project =() =>{
                         
                     ))}
                     </div>
+
+                </div>
+
+                <div className="p-6 rounded-xl border bordder-white/10 hover:translate-y hover:border-blue-500/30 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] transition-all">
+                    <h3 className="text-xl font-bold mb-2">B-DAG.PH</h3>
+                    <p className="text-gray-400 mb-4" >The primary objective of this system is to modernize Dagupan City's public transportation system by introducing a 
+                        technology-driven solution
+                    </p>
+                    <div>
+                        {["PHP", "TailwindCSS" ,"MySQL"].map((skill, key) => (
+                        <span key={key} className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm 
+                                hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2) ] transition">{skill}</span>
+                        
+                    ))}
+                    </div>
+
+                    
+
+                </div>
+
+                <div className="p-6 rounded-xl border bordder-white/10 hover:translate-y hover:border-blue-500/30 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] transition-all">
+                    <h3 className="text-xl font-bold mb-2">TaskWise</h3>
+                    <p className="text-gray-400 mb-4" >TaskWise is a to-do list application that helps users organize, prioritize, and manage tasks efficiently. 
+                        It allows users to create tasks, set deadlines, mark tasks as completed, and track daily progress. 
+                    </p>
+                    <div>
+                        {["Python"].map((skill, key) => (
+                        <span key={key} className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm 
+                                hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2) ] transition">{skill}</span>
+                        
+                    ))}
+                    </div>
+
+                    
 
                 </div>
 
